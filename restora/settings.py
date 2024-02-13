c505bf91a2b7b559c761e7158bda9db5a0cf077e
anidxn <anidxn215@gmail.com>
2024-02-13 06:50:30 +0530

    done

--- restora/settings.py
+++ restora/settings.py
@@ -142,6 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'code.zpd01@gmail.com'
-EMAIL_HOST_PASSWORD = 'zwiwzfngseacnikk' #'/At@rei8X'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
